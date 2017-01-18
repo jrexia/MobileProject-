96c1b3f439dffcefc2c01c93b32771527bd5a37a
jrexia <jrexia@163.com>
2017-01-18 09:17:09 +0800

    首页部分
    导航条加商品轮播加商品列表

---
+++ js/index.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    var mySwiper = new Swiper('.swiper-container', {
+        direction: 'horizontal',
+        loop: true,
+        autoplay: 2000,
+        autoplayDisableOnInteraction: 'true',
+
+
+    })
+}
