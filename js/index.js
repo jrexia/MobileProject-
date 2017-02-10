81d9faaa80f8d8a3144dcd0d3400977c4217753a
jrexia <jrexia@163.com>
2017-02-10 13:05:44 +0800

    版本1

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 window.onload = function() {
     var mySwiper = new Swiper('.swiper-container', {
-        direction: 'horizontal',
-        loop: true,
-        autoplay: 2000,
-        autoplayDisableOnInteraction: 'true',
-
-
-    })
+            direction: 'horizontal',
+            loop: true,
+            autoplay: 2000,
+            autoplayDisableOnInteraction: 'true',
+        })
+        // $("#nav #nav-4").click(function() {
+        //     $("#nav #nav-4 a span").addClass("active");
+        // })
 }
